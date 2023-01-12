fe7075c9f88cfe6db9de8a8e7dbba6fd3b3478a1
Theo <poette.theo@gmail.com>
2023-01-11 20:49:57 -0500

    feat: ssg with test posts

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,35 +1,32 @@
 import * as React from "react";
 import Link from "next/link";
-/* import { graphql, useStaticQuery } from "gatsby" */
-/* import { StaticImage, GatsbyImage, getImage } from "gatsby-plugin-image" */
 import Image from "next/image";
+import { getPostBySlug, getSortedPostsData } from "../../../lib/posts";
+import { useRouter } from "next/router";
+import { GetStaticPaths } from "next";
 
 const BlogPost = ({ post }: any) => {
-  /* const image = getImage(post.node.Cover?.localFile) */
-  const image = post.image;
+  const image = post?.image;
   return (
     <div className="flex items-center justify-center p-3">
-      <Link href={`/post/${post.node.Slug}/`}>
+      <Link href={`/post/${post?.slug}/`}>
         <div className="cursor-pointer rounded-2xl bg-white shadow-xl ring-1 ring-gray-200">
           <div className="grid grid-flow-row sm:grid-flow-col sm:grid-cols-5 sm:gap-8">
             {/* cover image of the post */}
             {image && (
               <Image
                 className="rounded-t-2xl sm:col-span-2 sm:rounded-l-2xl sm:rounded-r-none"
-                /* image={image} */
-                alt="Blog post cover image"
                 src={image}
-                width={400}
-                height={250}
+                alt="Blog post cover image"
               />
             )}
             {/* title and description of the post */}
             <div className="prose-md flex w-full flex-col space-y-2 p-4 sm:col-span-3">
               <h1 className="text-3xl font-bold tracking-tight">
-                {post.node.Title}
+                {/* {post.node.Title} */}
               </h1>
               <p className="flex h-full items-center align-middle font-medium text-gray-700">
-                {post.node.Description}
+                {/* {post.node.Description} */}
               </p>
             </div>
           </div>
@@ -38,5 +35,27 @@ const BlogPost = ({ post }: any) => {
     </div>
   );
 };
+
+export const getStaticPaths: GetStaticPaths<{ slug: string }> = async () => {
+  const paths = getSortedPostsData();
+  let prerender: any = [];
+  paths.map((path: any) => {
+    prerender.push({ params: { slug: path.slug } });
+  });
+  console.log(prerender, "prerender");
+  return {
+    paths: prerender,
+    fallback: false,
+  };
+};
+
+export async function getStaticProps(context: any) {
+  const post = getPostBySlug(context.slug);
+  return {
+    props: {
+      post,
+    },
+  };
+}
 
 export default BlogPost;
---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import Post from "../posts/[slug]";
+import SearchBar from "../components/SearchBar";
+import { getSortedPostsData } from "../../../lib/posts";
+
+const AllPosts = ({ allPostsData }: any) => {
+  const [blogPosts, setBlogPosts] = useState(allPostsData);
+
+  const displayPosts = () => {
+    if (blogPosts.length > 0) {
+      return blogPosts.map((post: any, index: number) => (
+        <Post key={index} post={post} />
+      ));
+    } else {
+      return (
+        <div className="flex items-center justify-center">
+          <p className="prose-xl font-semibold">No blog post found.</p>
+        </div>
+      );
+    }
+  };
+
+  const filterPostsFromSearchBar = (filter: string) => {
+    const filteredPosts = allPostsData.filter((post: any) => {
+      return post.title.toLowerCase().includes(filter.toLowerCase());
+    });
+    setBlogPosts(filteredPosts);
+  };
+
+  return (
+    <>
+      <SearchBar filterPosts={filterPostsFromSearchBar} />
+      {displayPosts()}
+      <div className="pb-8"></div>
+    </>
+  );
+};
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default AllPosts;
